8b6c70b1350a46e0511894f0271396546f12feab
Grant Willison <emailgdw@gmail.com>
2017-07-10 22:51:13 -0700

    Message class almost done

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Toolbar from './toolbar';
+import Message from './message';
+
+const list= [
+  {
+    id: 1,
+    labels: ['dev', 'personal'],
+    message: 'You can\'t input the protocol without calculating the mobile RSS protocol!',
+    starred: false,
+    read: false,
+    checked: false
+  },
+  {
+    id: 2,
+    labels: ['dev', 'personal'],
+    message: 'connecting the system won\'t do anything, we need to nput the mobile AI panel!',
+    starred: false,
+    read: false,
+    checked: true
+    },
+  {
+      id: 4,
+      labels: [],
+      message: 'Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!',
+      starred: false,
+      read: false,
+      checked: false
+  },
+  {
+    id: 3,
+    labels: ['dev'],
+    message: 'we need to program the primary TCP hard drive!',
+    starred: true,
+    read: true,
+    checked: true
+  },
+  {
+    id: 5,
+    labels: ['personal'],
+    message: 'If we override the interface, we can get to the HTTPfeed through the virtual EXE interface',
+    starred: false,
+    read: false,
+    checked: false
+  },
+  {
+    id: 6,
+    labels: [],
+    message: 'We need to back up the wireless GB driver!',
+    starred: false,
+    read: true,
+    checked: false,
+  },
+  {
+    id: 7,
+    labels: [],
+    message: 'We need to index the mobile PCI bus!',
+    starred: true,
+    read: true,
+    checked: false
+  },
+  {
+    id: 8,
+    labels: ['dev', 'personal'],
+    message: 'If we connect the sensor, we can get to the HDD port throught the redundant IB firewall!',
+    starred: false,
+    read: true,
+    checked: false
+    },
+];
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Toolbar />
+          {list.map((message) => {
+            return   <Message key={message.id} id={message.id} labels={message.labels} message={message.message} starred={message.starred} checked={message.checked}
+            read={message.read}/>
+          })}
+
+      </div>
+
+    );
+  }
+}
+
+export default App;
